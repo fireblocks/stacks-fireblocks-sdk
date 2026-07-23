ecb97bbf7ff4403bf31dfbb0b2b9fd45287263fb
tomer-shoham <tshoham@fireblocks.com>
2026-07-23 10:59:57 +0300

    feat: add note and externalId parameters to executeAction method in ApiService

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -181,6 +181,8 @@ export class ApiService {
             params.newRecipient,
             params.newAmount,
             params.nonceOverride,
+            params.note,
+            params.externalId,
           );
           break;
         case ActionType.GET_ACCOUNT_NONCE:
